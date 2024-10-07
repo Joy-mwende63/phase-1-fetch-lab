4d0fae85c6acb98b2c189c4bc8fbb46810dc217e
Joy-mwende63 <mwendejoy47@gmail.com>
2024-10-07 14:50:35 +0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  const apiUrl = 'https://anapioficeandfire.com/api/books';
   
+  return fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); 
+    })
+    .then(data => {
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
